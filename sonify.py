83371a90eac0e36ca01c1c5de5fb573bdbde8c91
lucas <lucas@thestrangeagency.com>
2020-07-05 12:38:46 +0200

    normalize

--- sonify.py
+++ sonify.py
@@ -1,10 +1,8 @@
 import math
-import random
-import struct
 import wave
+from pathlib import Path
 
 from PIL import Image
-from pathlib import Path
 
 
 class Osc:
@@ -16,7 +14,7 @@ class Osc:
     def step(self):
         self.phi += 1
         w = 2 * math.pi * self.freq / 44100.0
-        return int(self.amp * 32767.0 * math.sin(w * self.phi))
+        return self.amp * math.sin(w * self.phi)
 
 
 class Scanner:
@@ -25,43 +23,48 @@ class Scanner:
         self.freq = freq
         self.osc = Osc(freq, 0)
 
-# array of channels
-# in each channel, define column to sample as portion of image width
-scanners = [
-        [
-            Scanner(.2, 660),
-            Scanner(.3, 440),
-            Scanner(.4, 220),
-            Scanner(.5, 110),
-        ],
-        [
-            Scanner(.5, 110),
-            Scanner(.6, 450),
-            Scanner(.7, 230),
-            Scanner(.8, 670),
-        ]
-    ]
 
-audio_buffer = bytearray()
+class Channel:
+    def __init__(self, scanners):
+        self.scanners = scanners
+        self.buffer = []
+
+
+l_channel = Channel([
+    Scanner(.2, 660),
+    Scanner(.3, 440),
+    Scanner(.4, 220),
+    Scanner(.5, 110),
+])
+
+r_channel = Channel([
+    Scanner(.5, 110),
+    Scanner(.6, 450),
+    Scanner(.7, 230),
+    Scanner(.8, 670),
+])
+
+channels = [l_channel, r_channel]
+loudest = 0  # for normalization
 
 # path = Path("../temp/rnd_point_19c/out_a")
 path = Path("images")
 files = sorted(path.glob('*.png'))
 
 for file in files:
-    
+
     print(file)
 
     image = Image.open(file)
     pixels = image.load()
     w, h = image.size
-    
+
     print(pixels[w/2, h/2])
 
-    for channel in scanners:
+    for channel in channels:
 
-        for scanner in channel:
-        
+        for scanner in channel.scanners:
+
             # calculate amplitude at this image location
             amp = 0
             for y in range(h):
@@ -75,15 +78,27 @@ for file in files:
 
     # fill 44100/30=1470 frames of the audio buffer
     for step in range(1470):
-        for channel in scanners:
+        for channel in channels:
             sample = 0
-            for scanner in channel:
+            for scanner in channel.scanners:
                 sample += scanner.osc.step()
-            byted = (sample).to_bytes(2, byteorder='little', signed=True)
-            audio_buffer += byted
+            if sample > loudest:
+                loudest = sample
+            channel.buffer.append(sample)
 
-wav = wave.open('tone_03.wav', 'w')
+
+# convert, normalize, and interleave channel buffers to byte array
+audio_buffer = bytearray()
+normfactor = .9 / loudest
+for step in range(len(channels[0].buffer)):
+    for channel in channels:
+        sample = int(normfactor * 32767.0 * channel.buffer[step])
+        byted = sample.to_bytes(2, byteorder='little', signed=True)
+        audio_buffer += byted
+
+
+# write to wav file
+wav = wave.open('sonics.wav', 'w')
 wav.setparams((2, 2, 44100, 0, 'NONE', 'not compressed'))
-
 wav.writeframes(audio_buffer)
 wav.close()
