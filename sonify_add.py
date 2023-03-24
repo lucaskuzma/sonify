4441b89d2934c5c7550e03bed9c2f997c0a529b1
Lucas Kuzma <lucas@lucaskuzma.com>
2023-03-24 09:53:04 +0100

    use tqdm for progress

--- sonify_add.py
+++ sonify_add.py
@@ -2,6 +2,8 @@ import math
 import wave
 from pathlib import Path
 import click
+from tqdm import trange
+import itertools
 from PIL import Image
 
 """
@@ -37,58 +39,54 @@ def main(path, scan_size):
     files = sorted(path.glob("*.png"))
     phi = 0
     count = len(files) - 1
-    print(f"{count} files to process")
+    print(f"{count} files to process in `{path}`")
 
     n_oscillators = scan_size * scan_size
     print(f"{n_oscillators} oscillators activated")
 
-    with click.progressbar(range(count), label="Files", length=count) as bar:
-        for frame in bar:
-            # load current and next image
-            file_1 = files[frame]
-            file_2 = files[frame + 1]
+    for frame in trange(count):
+        # load current and next image
+        file_1 = files[frame]
+        file_2 = files[frame + 1]
 
-            image_1 = Image.open(file_1).resize(
-                (scan_size, scan_size), resample=Image.BICUBIC
-            )
-            image_2 = Image.open(file_2).resize(
-                (scan_size, scan_size), resample=Image.BICUBIC
-            )
+        image_1 = Image.open(file_1).resize(
+            (scan_size, scan_size), resample=Image.BICUBIC
+        )
+        image_2 = Image.open(file_2).resize(
+            (scan_size, scan_size), resample=Image.BICUBIC
+        )
 
-            pixels_1 = image_1.load()
-            pixels_2 = image_2.load()
+        pixels_1 = image_1.load()
+        pixels_2 = image_2.load()
 
-            w, h = image_1.size
+        w, h = image_1.size
 
-            # iterate over samples in this frame
-            for sample_index in range(samples_per_frame):
-                l_sample = r_sample = 0
-                # iterate over image pixels
-                for x in range(scan_size):
-                    for y in range(scan_size):
-                        amp_1 = pixels_1[x, y][0] / 256  # 0..1
-                        amp_2 = pixels_2[x, y][0] / 256  # 0..1
+        # iterate over samples in this frame
+        for sample_index in trange(samples_per_frame, leave=False):
+            l_sample = r_sample = 0
+            # iterate over image pixels
+            for x, y in itertools.product(range(scan_size), repeat=2):
+                amp_1 = pixels_1[x, y][0] / 256  # 0..1
+                amp_2 = pixels_2[x, y][0] / 256  # 0..1
 
-                        # linterp
-                        amp = amp_1 + (amp_2 - amp_1) * (
-                            sample_index / samples_per_frame
-                        )
+                # linterp
+                amp = amp_1 + (amp_2 - amp_1) * (sample_index / samples_per_frame)
 
-                        # calculate sample contribution from this pixel
-                        freq = (
-                            20 + 3500 * (scan_size - y) / scan_size
-                        )  # frequency based on y coordinate, 3520 is 440 * 8
-                        w = 2 * math.pi * freq / 44100.0
-                        sample = (
-                            amp * math.sin(w * phi) / n_oscillators
-                        )  # scale sample by amplitude and n_oscillators
+                # calculate sample contribution from this pixel
+                freq = (
+                    20 + 3500 * (scan_size - y) / scan_size
+                )  # frequency based on y coordinate, 3520 is 440 * 8
+                w = 2 * math.pi * freq / 44100.0
+                sample = (
+                    amp * math.sin(w * phi) / n_oscillators
+                )  # scale sample by amplitude and n_oscillators
 
-                        l_sample += sample * (1 - x / scan_size)
-                        r_sample += sample * (x / scan_size)
+                l_sample += sample * (1 - x / scan_size)
+                r_sample += sample * (x / scan_size)
 
-                l_channel.buffer.append(l_sample)
-                r_channel.buffer.append(r_sample)
-                phi += 1
+            l_channel.buffer.append(l_sample)
+            r_channel.buffer.append(r_sample)
+            phi += 1
 
     # convert, normalize, and interleave channel buffers to byte array
     audio_buffer = bytearray()
